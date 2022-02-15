ca1b006076897065555c56ad32470fd88b7655fd
Steve <40363845+stvemillertime@users.noreply.github.com>
2022-02-14 23:42:08 -0500

    Update cerebro-string-basic.py
    Added frenchstack

--- cerebro-string-mutations/cerebro-string-basic.py
+++ cerebro-string-mutations/cerebro-string-basic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import binascii
 from os import error
-import sys
+import sys, string, struct
 from datetime import date
 import argparse
 import os.path
@@ -10,7 +10,6 @@ import encodings
 import fileinput
 import re
 import base64
-import string
 
 '''
 .d88b 8888 888b. 8888 888b. 888b. .d88b. 
@@ -25,7 +24,118 @@ If this doesn't work, because of your Python install and paths... make sure you'
 Example input: a string such as "VirtualAlloc"
 Example output: a list of mutations of those strings such as $a01_VirtualAlloc = "hllochualAhVirt" nocase ascii wide
 
+Usage:
+
+python3 cerebro-string-basic.py -s kernel32.dll -m frenchstack
+python3 cerebro-string-basic.py --str GetCurrentProcess --mut hex 
+python3 cerebro-string-basic.py -s IsDebuggerPresent --mut a
+ll
+
 '''
+
+# frenchstack stuff below courtesy of @notareverser https://gist.github.com/notareverser/4f6b9c644d4fe517889b3fbb0b4271ca
+
+def strByByte(_strval):
+    strval = bytearray(_strval.encode())
+    for s in strval: yield s
+
+def strByDword(_strval):
+    strval = bytearray(_strval.encode())
+    numDwords = int(len(strval) / 4)
+    remainder = len(strval) - (numDwords*4)
+
+    for x in range(numDwords):
+        yield struct.unpack_from("<L", strval, 4*x)[0]
+    if remainder != 0:
+        remainderVals = strval[4*numDwords:]
+        remainderVals.extend(bytearray([0]*(4-remainder)))
+        yield struct.unpack("<L", remainderVals)[0]
+
+        
+def hexlify(v): return '{:x}'.format(v)
+def strToHex(strval): return ''.join(list(map(lambda k: '{:02x}'.format(ord(k)), strval)))
+def bufToHex(bval): return ''.join(list(map(lambda k: '{:02x}'.format(k), bval)))
+
+
+    
+def doPrototype(proto, _strval):
+    strval = bytearray(_strval.encode())
+    v = []
+    for s in strval: v.append(proto.format(s))
+    return ' '.join(v)
+
+def generateSmallStack(strval):
+    return '   $smallStack = {' + doPrototype('c645??{:02x}', strval) +'}'
+
+def generateLargeStack(strval):
+    return '   $largeStack = {' + doPrototype('c7(45|85)[1-4]{:02x}000000', strval) + '}'
+
+def generateRegister(strval):
+    return '   $register = {'+ doPrototype('b?{:02x}000000 6689????', strval) + '}'
+
+def generateDword(_strval):
+    strval = bytearray(_strval.encode())
+    numDwords = int(len(strval)/4)
+    remainder = len(strval) - (numDwords*4)
+
+    values = []
+    for x in range(numDwords):
+        nextd = struct.unpack_from("<L", strval, 4*x)[0]
+        values.append('c7(45|85)[1-4]{:08x}'.format(nextd))
+    if remainder > 0:
+        if remainder == 3:
+            firstv, secondv = struct.unpack("<HB", strval[-remainder:])
+            print(firstv, secondv, "oops - something bad happened in generateDword")
+            #print(firstv, secondv,file=sys.stderr)
+            values.append('[0-1]c7(45|85)[1-4]{:04x}'.format(firstv))
+            values.append('[0-1]c6(45|85)[1-4]{:02x}'.format(secondv))
+        elif remainder == 2:
+            firstv = struct.unpack("<H", strval[-remainder:])[0]
+            values.append('[0-1]c7(45|85)[1-4]{:04x}'.format(firstv))
+        elif remainder == 1:
+            values.append('[0-1]c6(45|85)[1-4]{:02x}'.format(strval[-1]))
+    
+    return '   $dword = {'+(' '.join(values))+ '}'
+
+def generatePushPop(strval):
+    return ('   $pushpop = {'+
+            doPrototype('6a{:02x}5? ', strval[0]) +
+            doPrototype('6a{:02x} 6689????5?', strval[1:-1]) + '}')
+            
+def generateCallOverString(strval):
+    return ('   $callOverString = {e8'+
+            bufToHex(struct.pack("<L", len(strval))) +
+            strToHex(strval) + '5? }')
+
+def fixName(strval):
+    result = []
+    valid = string.ascii_letters + string.digits + '_'
+    for s in strval:
+        result.append('_' if s not in valid else s)
+    return ''.join(result)
+
+def generate(strval):
+    nl = '\n'
+    clauses = []
+    clauses.append("rule stackstring_{}".format(fixName(strval)))
+    clauses.append("{")
+    clauses.append("  strings:")
+    
+    clauses.append(generateSmallStack(strval)+nl)
+    clauses.append(generateLargeStack(strval)+nl)
+    clauses.append(generateRegister(strval)+nl)
+    clauses.append(generateDword(strval)+nl)
+    clauses.append(generatePushPop(strval)+nl)
+    clauses.append(generateCallOverString(strval)+nl)
+    
+    clauses.append("  condition:")
+    clauses.append("    any of them")
+    clauses.append("}")
+    
+    print(nl.join(clauses)+nl+nl)
+
+# frenchstack stuff above courtesy of @notareverser https://gist.github.com/notareverser/4f6b9c644d4fe517889b3fbb0b4271ca
+
   
 def make_flipflop_strings(thing):
     try:
@@ -139,7 +249,7 @@ def main_active(args = sys.argv[1:]):
     group.add_argument('-s','--str', type=str, help='Single string to mutate.')
    
     # Mutation selection choices.
-    parser.add_argument('-m','--mut','--mutation', choices=['flipflop','reverse','stackpush','stackpushnull','stackpushdoublenull','hex','fallchill','all'], type=str, required=True)
+    parser.add_argument('-m','--mut','--mutation', choices=['flipflop','reverse','stackpush','stackpushnull','stackpushdoublenull','hex','fallchill','all','frenchstack'], type=str, required=True)
 
     args = parser.parse_args(args)
 
@@ -181,7 +291,9 @@ def main_active(args = sys.argv[1:]):
                     mut_type = "_hex_enc_str"
                     mutated_str = make_hex_encoded_strings(in_string)
                     assemble_output(clean_str,mut_type,mutated_str)
-                elif mutation == "all":
+                elif mutation == "frenchstack":
+                    generate(in_string)
+                elif mutation == "all": #everything but frenchstack
                     funcs = [
                             make_flipflop_strings(in_string),
                             make_reverse_strings(in_string),
